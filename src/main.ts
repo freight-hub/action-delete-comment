a0e7e9f61d1c0a1a8d46a80ad5068860cdb7e976
Rodolfo Matos <rodolfo.matos@forto.com>
2022-01-31 18:12:09 +0100

    0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from "@actions/github";
+import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const githubToken = core.getInput("github_token", { required: true });
+    const body = core.getInput("body", { required: true });
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(githubToken);
 
-    core.setOutput('time', new Date().toTimeString())
+    let { owner, repo } = github.context.repo;
+    if (core.getInput("repo")) {
+      [owner, repo] = core.getInput("repo").split("/");
+    }
+
+    const number = core.getInput("number") === ""
+      ? github.context.issue.number
+      : parseInt(core.getInput("number"));
+
+    const comments = await octokit.issues.listComments({
+      owner,
+      repo,
+      issue_number: number,
+    });
+    const comment_ids = comments.data.filter((comment) =>
+      comment.body?.startsWith(body)
+    ).map((comment) => comment.id);
+    for (const comment_id of comment_ids) {
+      await octokit.issues.deleteComment({
+        owner,
+        repo,
+        issue_number: number,
+        comment_id,
+      });
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.error(error);
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
